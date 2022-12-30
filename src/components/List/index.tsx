4d02a1cd8f0de5b1eca8c59170c887c39b63015a
Marcos Maciel <m.maciel07@hotmail.com>
2022-12-30 11:49:12 -0300

    first commit

---
+++ src/components/List/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import style from './List.module.scss'
+
+function List() {
+    const tasks = [
+        {
+            tarefa: "React",
+            tempo: "02:00:00"
+        },
+        {
+            tarefa: "JavaScript",
+            tempo: "01:00:00"
+        },
+        {
+            tarefa: "TypeScript",
+            tempo: "03:00:00"
+        },
+    ]
+    return (
+        <aside className={style.listaTarefas}>
+            <h2>Estudos do dia</h2>
+            <ul>
+                {tasks.map((item, i) => (
+                    <li className="item" key={i}>
+                        <h3>{item.tarefa}</h3>
+                        <span>{item.tempo}</span>
+                    </li>
+                ))}
+            </ul>
+        </aside>
+    )
+}
+
+export default List;
